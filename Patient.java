abaa1022400e6264537ada67373c9c446658c2d4
Vibhor <bijoy.vibhor19@gmail.com>
2017-04-29 21:12:40 +0530

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -11,23 +11,37 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Info extends AppCompatActivity {
 
     EditText firstname,lastname,age,city;
     TextView textView;
     DB_Controller controller;
+    List<String> cities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-
+        initCities();
         firstname = (EditText)findViewById(R.id.et_f);
         lastname = (EditText)findViewById(R.id.et_l);
         age = (EditText)findViewById(R.id.age);
         city = (EditText) findViewById(R.id.et_city);
         textView = (TextView)findViewById(R.id.textView2);
         controller = new DB_Controller(this,"",null,1);
+    }
+
+    private void initCities(){
+        cities = new ArrayList<>();
+        cities.add("kolkata");
+        cities.add("new delhi");
+        cities.add("bombay");
+        cities.add("chandigarh");
+        cities.add("bengaluru");
+        // add all the desired cities here
     }
 
     private void emptyEditTexts(){
@@ -37,10 +51,18 @@ public class Info extends AppCompatActivity {
         city.setText(null);
     }
 
+    private boolean isValidCity(){
+        if(city.getText().toString().isEmpty() || !cities.contains(city.getText().toString().toLowerCase()))
+            return false;
+        return true;
+    }
+
     public void btn_click(View view) {
         switch (view.getId()){
             case R.id.b_add:
                 try {
+                    if(!isValidCity())
+                        return;
                     controller.insert_patient(firstname.getText().toString(),lastname.getText().toString(),age.getText().toString(),city.getText().toString());
                     Toast.makeText(this, "Patient added successfully!", Toast.LENGTH_SHORT).show();
                     emptyEditTexts();
