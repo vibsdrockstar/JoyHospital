f3922e381143887012370d09109ab08ed8208219
Vibhor <bijoy.vibhor19@gmail.com>
2017-04-30 13:04:55 +0530

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -8,10 +8,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Patients extends AppCompatActivity {
     EditText patientname, epidemic, cure, fee, status;
     TextView textView;
     DB3_Controller controller;
+    List<String> disease;
+    List<String> treatment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,25 @@ public class Patients extends AppCompatActivity {
         controller = new DB3_Controller(this, "", null, 1);
 
     }
+    private void initDisease(){
+        disease=new ArrayList<>();
+        disease.add("cough");
+        disease.add("eyeflu");
+        disease.add("cancer");
+        disease.add("catract");
+        disease.add("aneurysm");
+        disease.add("osteoarthritis");
+        //add all desired diseases
+    }
+    private void  initTreatment(){
+        treatment=new ArrayList<>() ;
+        treatment.add("honitus");
+        treatment.add("eyedrops");
+        treatment.add("chemotheraphy");
+        treatment.add("surgery");
+        treatment.add("openheartsurgery");
+        //add relevant treatment
+    }
 
     private void emptyEditTexts() {
         patientname.setText(null);
@@ -34,11 +58,24 @@ public class Patients extends AppCompatActivity {
         fee.setText(null);
         status.setText(null);
     }
-
+   private boolean isValidDisease(){
+       if (epidemic.getText().toString().isEmpty() || !disease.contains(epidemic.getText().toString().toLowerCase()))
+           return false;
+       return true;
+   }
+    private boolean isValidTreatment(){
+        if (cure.getText().toString().isEmpty() || !treatment.contains(cure.getText().toString().toLowerCase()))
+            return false;
+        return true;
+    }
     public void btn_click(View view) {
         switch (view.getId()) {
             case R.id.b_add:
                 try {
+                    if (!isValidDisease())
+                        return;
+                    if (!isValidTreatment())
+                        return;
                     controller.insert_info(patientname.getText().toString(), epidemic.getText().toString(), cure.getText().toString(), fee.getText().toString(), status.getText().toString());
                     Toast.makeText(this, "Information added successfully!", Toast.LENGTH_SHORT).show();
                     emptyEditTexts();
